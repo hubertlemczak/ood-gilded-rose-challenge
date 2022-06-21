9e2effe02d6528d1512867eff0cebd645670e4eb
Hubert Lemczak <hubertlemczak@gmail.com>
2022-06-21 18:41:33 +0100

    added explanation comments

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -1,7 +1,9 @@
 class Item {
   constructor(name, sellIn, quality) {
     this.name = name;
+    // days to sell item
     this.sellIn = sellIn;
+    // changes every day either + or -
     this.quality = quality;
   }
 }
@@ -12,25 +14,26 @@ class Shop {
   updateQuality() {
     //cycling through all items
     for (var i = 0; i < this.items.length; i++) {
-      if (
-        this.items[i].name != 'Aged Brie' &&
-        this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert'
-      ) {
+      if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
         if (this.items[i].quality > 0) {
+          // 'Sulfuras, Hand of Ragnaros' properties never change
           if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
             this.items[i].quality = this.items[i].quality - 1;
           }
         }
       } else {
+        // if 'Aged Brie' or 'Backstage passes to a TAFKAL80ETC concert'
         if (this.items[i].quality < 50) {
           this.items[i].quality = this.items[i].quality + 1;
           // Checks for ticket
           if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
+            // if concert within 10 days the quality increases
             if (this.items[i].sellIn < 11) {
               if (this.items[i].quality < 50) {
                 this.items[i].quality = this.items[i].quality + 1;
               }
             }
+            // if concert within 5 days the quality increases even more
             if (this.items[i].sellIn < 6) {
               if (this.items[i].quality < 50) {
                 this.items[i].quality = this.items[i].quality + 1;
@@ -39,6 +42,7 @@ class Shop {
           }
         }
       }
+      // 'Sulfuras, Hand of Ragnaros' properties never change
       if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
         this.items[i].sellIn = this.items[i].sellIn - 1;
       }
@@ -46,14 +50,17 @@ class Shop {
         if (this.items[i].name != 'Aged Brie') {
           if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
             if (this.items[i].quality > 0) {
+              // 'Sulfuras, Hand of Ragnaros' properties never change
               if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
                 this.items[i].quality = this.items[i].quality - 1;
               }
             }
+            // if item is 'Backstage passes to a TAFKAL80ETC concert' quality will be 0 as passed it's sellIn date
           } else {
             this.items[i].quality = this.items[i].quality - this.items[i].quality;
           }
         } else {
+          // aged brie quality will keep increasing even after sellIn date
           if (this.items[i].quality < 50) {
             this.items[i].quality = this.items[i].quality + 1;
           }
