e5ec0d34d78f1d6366bd12c9cfd79cbc2f1989f0
Hubert Lemczak <hubertlemczak@gmail.com>
2022-06-21 16:29:15 +0100

    init commit

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -1,10 +1,9 @@
-var {Shop, Item} = require('../src/gilded_rose.js');
-describe("Gilded Rose", function() {
+const { Shop, Item } = require('../src/gilded_rose.js');
 
-  it("should foo", function() {
-    const gildedRose = new Shop([ new Item("foo", 0, 0) ]);
+describe('Gilded Rose', function () {
+  it('should foo', function () {
+    const gildedRose = new Shop([new Item('foo', 0, 0)]);
     const items = gildedRose.updateQuality();
-    expect(items[0].name).toEqual("fixme");
+    expect(items[0].name).toEqual('fixme');
   });
-
 });
--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -1,18 +1,20 @@
 class Item {
-  constructor(name, sellIn, quality){
+  constructor(name, sellIn, quality) {
     this.name = name;
     this.sellIn = sellIn;
     this.quality = quality;
   }
 }
-
 class Shop {
-  constructor(items=[]){
+  constructor(items = []) {
     this.items = items;
   }
   updateQuality() {
     for (var i = 0; i < this.items.length; i++) {
-      if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
+      if (
+        this.items[i].name != 'Aged Brie' &&
+        this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert'
+      ) {
         if (this.items[i].quality > 0) {
           if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
             this.items[i].quality = this.items[i].quality - 1;
@@ -62,5 +64,5 @@ class Shop {
 }
 module.exports = {
   Item,
-  Shop
-}
+  Shop,
+};
